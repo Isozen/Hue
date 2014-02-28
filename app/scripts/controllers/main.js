5b1c7ef817377b74a3ba2322cdfcfe0db20f8b99
Pierre Acces <pierre.acces@gmail.com>
2014-02-28 09:17:21 +0100

    Création du projet

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('hueApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
